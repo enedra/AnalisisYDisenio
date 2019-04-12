16ba063f1be47dba3c8adc21f0affe6788c03fd1
enedra <eduardo.pera.rg@hotmail.com>
2019-04-12 11:46:00 -0300

    meti maven pero no compila por spark

---
+++ pregunVet/src/main/java/com/pionono/app/App.java
@@ -0,0 +1,15 @@
+package com.pionono.app;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
